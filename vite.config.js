919133f532e8b023d50f68d4ca812c0f18f4acdd
Arpan Khare <kharearpan7777@gmail.com>
2025-09-06 12:23:32 +0530

    The first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: '/React_portfolio/',   // Add this line
   plugins: [react()],
-})
+  base: "/arpan-portfolio", 
+  
+});
+
